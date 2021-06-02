ae578a6cfef44e511a7e6d65627f4fce647c8cae
saidou diallo <saidou224dev@hotmail.com>
2021-06-02 17:12:58 +0000

    First commit to my mobile-app native

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
+  const[todos,setTodos]= useState([
+    {text:'mon frere est devs',key:'1'},
+    {text:'je suis  dev hybride',key:'2'},
+    {text:'oumar est mecanicien',key:'3'}
+  ]);
+
+  // bouton permettant de supprimer ou filtrer
+  const pressHandler=(key)=>{
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo=>todo.key !=key);
+    })
+  }
+  const submitHandler=(text)=>{
+    setTodos((prevTodos)=>{
+    return [
+      {text:text, key:Math.random().toString()},
+      ...prevTodos
+    ];
+  })
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    {/* debut header */}
+    <Header/>
+    <View style={styles.content}>
+    {/* to form */}
+    <AddTodo submitHandler={submitHandler}/>
+      <View style={styles.list}>
+      <FlatList 
+      data={todos}
+      renderItem={({item})=>(
+      <TodoItem  item={item} pressHandler={pressHandler}/>
+      )}
+      />
+      </View>
+    </View>
     </View>
   );
 }
@@ -15,7 +50,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  content: {
+    padding:40,
+  },
+  list:{
+  marginTop: 0,
+  }
 });
